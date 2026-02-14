7bb08b0bba1e7fa687d6823c169c139a99056e0e
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2026-02-14 18:07:23 +0400

    fixed stdin noise in spawned instances

--- src/agent_injector/server.py
+++ src/agent_injector/server.py
@@ -97,6 +97,7 @@ async def _spawn_task(
 
     proc = await asyncio.create_subprocess_exec(
         *args,
+        stdin=asyncio.subprocess.DEVNULL,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
         cwd=cwd,
